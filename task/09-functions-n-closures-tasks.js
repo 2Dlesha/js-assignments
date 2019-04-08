18fa2b580710fab152b5b9cf6e9d7d4694fb46fe
2Dlesha <44382262+2Dlesha@users.noreply.github.com>
2019-04-08 04:07:59 +0300

    Update 09-functions-n-closures-tasks.js

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -26,7 +26,7 @@
  *
  */
 function getComposition(f,g) {
-    throw new Error('Not implemented');
+    return function(x){return f(g(x))}
 }
 
 
@@ -47,7 +47,8 @@ function getComposition(f,g) {
  *
  */
 function getPowerFunction(exponent) {
-    throw new Error('Not implemented');
+    return function(x){return Math.pow(x,exponent)}
+    //throw new Error('Not implemented');
 }
 
 
